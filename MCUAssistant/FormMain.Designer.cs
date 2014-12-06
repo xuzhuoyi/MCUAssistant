e6f433f0d54360b628c81094577a65b6c758a8cb
xuzhuoyi <xzy476386434@vip.qq.com>
2014-12-06 19:41:35 +0800

    First Commit

---
+++ MCUAssistant/FormMain.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace MCUAssistant
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelMIPS = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelClkTime = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelMacTime = new System.Windows.Forms.Label();
+            this.textBoxus = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelABC = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxRes = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(43, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MCU Assistant";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(65, 182);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1004, 485);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBoxRes);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.labelABC);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBoxus);
+            this.tabPage1.Location = new System.Drawing.Point(4, 34);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(996, 447);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "延时计算";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 34);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(996, 447);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "定时器/计数器";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 34);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(996, 447);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "编程手册速查";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 34);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(996, 447);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "关于";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "每秒大约执行";
+            // 
+            // labelMIPS
+            // 
+            this.labelMIPS.AutoSize = true;
+            this.labelMIPS.Location = new System.Drawing.Point(174, 101);
+            this.labelMIPS.Name = "labelMIPS";
+            this.labelMIPS.Size = new System.Drawing.Size(82, 24);
+            this.labelMIPS.TabIndex = 3;
+            this.labelMIPS.Text = "label3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "晶振";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0.032768",
+            "1",
+            "1.8432",
+            "3.6864",
+            "4",
+            "5.5296",
+            "6",
+            "10",
+            "11.0592",
+            "12.9024",
+            "12",
+            "23.592",
+            "24",
+            "24.576"});
+            this.comboBox1.Location = new System.Drawing.Point(427, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 32);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "12";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(586, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "MHz";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(639, 104);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 28);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "12T";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(722, 102);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 28);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "6T";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(793, 99);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 28);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "4T";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(864, 101);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(65, 28);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.Text = "1T";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(936, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 24);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "时钟周期";
+            // 
+            // labelClkTime
+            // 
+            this.labelClkTime.AutoSize = true;
+            this.labelClkTime.Location = new System.Drawing.Point(1049, 107);
+            this.labelClkTime.Name = "labelClkTime";
+            this.labelClkTime.Size = new System.Drawing.Size(82, 24);
+            this.labelClkTime.TabIndex = 13;
+            this.labelClkTime.Text = "label8";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(940, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 24);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "机器周期";
+            // 
+            // labelMacTime
+            // 
+            this.labelMacTime.AutoSize = true;
+            this.labelMacTime.Location = new System.Drawing.Point(1053, 152);
+            this.labelMacTime.Name = "labelMacTime";
+            this.labelMacTime.Size = new System.Drawing.Size(82, 24);
+            this.labelMacTime.TabIndex = 15;
+            this.labelMacTime.Text = "label9";
+            // 
+            // textBoxus
+            // 
+            this.textBoxus.Location = new System.Drawing.Point(132, 44);
+            this.textBoxus.Name = "textBoxus";
+            this.textBoxus.Size = new System.Drawing.Size(100, 35);
+            this.textBoxus.TabIndex = 0;
+            this.textBoxus.Text = "1000";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelABC
+            // 
+            this.labelABC.AutoSize = true;
+            this.labelABC.Location = new System.Drawing.Point(396, 58);
+            this.labelABC.Name = "labelABC";
+            this.labelABC.Size = new System.Drawing.Size(82, 24);
+            this.labelABC.TabIndex = 2;
+            this.labelABC.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "延时时间";
+            // 
+            // textBoxRes
+            // 
+            this.textBoxRes.Location = new System.Drawing.Point(24, 127);
+            this.textBoxRes.Multiline = true;
+            this.textBoxRes.Name = "textBoxRes";
+            this.textBoxRes.Size = new System.Drawing.Size(539, 274);
+            this.textBoxRes.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 712);
+            this.Controls.Add(this.labelMacTime);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.labelClkTime);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelMIPS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelMIPS;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelClkTime;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelMacTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxus;
+        private System.Windows.Forms.Label labelABC;
+        private System.Windows.Forms.TextBox textBoxRes;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MCUAssistant/FormMain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MCUAssistant
+{
+    public partial class FormMain : Form
+    {
+        float cryFreq, clkTime;
+        int perMacTime;
+        int a, b, c;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            
+            setCal();
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setCal();
+        }
+
+        private void initRatio()
+        {
+            if (radioButton1.Checked) perMacTime=12;
+            if (radioButton2.Checked) perMacTime=6;
+            if (radioButton3.Checked) perMacTime=4;
+            if (radioButton4.Checked) perMacTime=1;
+        }
+
+        private void setCal()
+        {
+            initRatio();
+            cryFreq = float.Parse(comboBox1.Text);
+            clkTime = 1 / cryFreq;
+            labelClkTime.Text = clkTime + "us";
+            labelMacTime.Text = clkTime * perMacTime + "us";
+            labelMIPS.Text = 1 / (clkTime * perMacTime)*100 + "万条指令";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            setCal();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            setCal();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            setCal();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            setCal();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calc(int.Parse(textBoxus.Text));
+            labelABC.Text = a +"a"+ b+"b" + c+"c";
+            textBoxRes.Text = "void delay(void)\r\n{\r\n    unsigned char a,b,c;\r\n    for(c=" + c + ";c>0;c--)\r\n        for(b=" + b + ";b>0;b--)\r\n            for(a=" + a + ";a>0;a--);\r\n}\r\n";
+
+        }
+
+        private void Calc(int time)
+        {
+            
+            for (a = 1; a < 256; a++)
+                for (b = 1; b < 256; b++)
+                    for (c = 1; c < 256; c++)
+                    {
+                        if (Math.Abs(2 * a * b * c + 3 * b * c + 3 * c + 3 - time) < 2) return; 
+                    }
+        }
+    }
+}
