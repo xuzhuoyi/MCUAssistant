db8b72f86427cb7bc10dfc0ab464dd4da4da1f7b
xuzhuoyi <xzy476386434@vip.qq.com>
2015-07-12 13:47:02 +0800

    Fix Crash While No Serial

--- MCUAssistant/FormMain.cs
+++ MCUAssistant/FormMain.cs
@@ -14,18 +14,18 @@ namespace MCUAssistant
 {
     public partial class FormMain : Form
     {
-        float cryFreq, clkTime;
-        int perMacTime;
-        int a, b, c;
-        Res r1, r2, r3, r4, r5;
+        float _cryFreq, _clkTime;
+        int _perMacTime;
+        int _a, _b, _c;
+        Res _r1, _r2, _r3, _r4, _r5;
         SerialPort sp1 = new SerialPort();
         //sp1.ReceivedBytesThreshold = 1;//只要有1个字符送达端口时便触发DataReceived事件
 
         struct Res
         {
-            public double value;
-            public bool enable;
-            public int unit;
+            public double Value;
+            public bool Enable;
+            public int Unit;
         }
 
         public FormMain()
@@ -36,72 +36,72 @@ namespace MCUAssistant
         private void FormMain_Load(object sender, EventArgs e)
         {
             
-            setCal();
-            initOmegaOption();
-            initSerialSetup();
+            SetCal();
+            InitOmegaOption();
+            InitSerialSetup();
 
             
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            setCal();
+            SetCal();
         }
 
-        private void initRatio()
+        private void InitRatio()
         {
-            if (radioButton1.Checked) perMacTime=12;
-            if (radioButton2.Checked) perMacTime=6;
-            if (radioButton3.Checked) perMacTime=4;
-            if (radioButton4.Checked) perMacTime=1;
+            if (radioButton1.Checked) _perMacTime=12;
+            if (radioButton2.Checked) _perMacTime=6;
+            if (radioButton3.Checked) _perMacTime=4;
+            if (radioButton4.Checked) _perMacTime=1;
         }
 
-        private void setCal()
+        private void SetCal()
         {
-            initRatio();
-            cryFreq = float.Parse(comboBox1.Text);
-            clkTime = 1 / cryFreq;
-            labelClkTime.Text = clkTime + "us";
-            labelMacTime.Text = clkTime * perMacTime + "us";
-            labelMIPS.Text = 1 / (clkTime * perMacTime)*100 + "万条指令";
+            InitRatio();
+            _cryFreq = float.Parse(comboBox1.Text);
+            _clkTime = 1 / _cryFreq;
+            labelClkTime.Text = _clkTime + "us";
+            labelMacTime.Text = _clkTime * _perMacTime + "us";
+            labelMIPS.Text = 1 / (_clkTime * _perMacTime)*100 + "万条指令";
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            setCal();
+            SetCal();
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            setCal();
+            SetCal();
         }
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            setCal();
+            SetCal();
         }
 
         private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
-            setCal();
+            SetCal();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Calc(int.Parse(textBoxus.Text));
-            labelABC.Text = a +"a"+ b+"b" + c+"c";
-            textBoxRes.Text = "void delay(void)\r\n{\r\n    unsigned char a,b,c;\r\n    for(c=" + c + ";c>0;c--)\r\n        for(b=" + b + ";b>0;b--)\r\n            for(a=" + a + ";a>0;a--);\r\n}\r\n";
+            labelABC.Text = _a +"a"+ _b+"b" + _c+"c";
+            textBoxRes.Text = "void delay(void)\r\n{\r\n    unsigned char a,b,c;\r\n    for(c=" + _c + ";c>0;c--)\r\n        for(b=" + _b + ";b>0;b--)\r\n            for(a=" + _a + ";a>0;a--);\r\n}\r\n";
 
         }
 
         private void Calc(int time)
         {
             
-            for (a = 1; a < 256; a++)
-                for (b = 1; b < 256; b++)
-                    for (c = 1; c < 256; c++)
+            for (_a = 1; _a < 256; _a++)
+                for (_b = 1; _b < 256; _b++)
+                    for (_c = 1; _c < 256; _c++)
                     {
-                        if (Math.Abs(2 * a * b * c + 3 * b * c + 3 * c + 3 - time) < 2) return; 
+                        if (Math.Abs(2 * _a * _b * _c + 3 * _b * _c + 3 * _c + 3 - time) < 2) return; 
                     }
         }
 
@@ -118,67 +118,67 @@ namespace MCUAssistant
         private void button2_Click(object sender, EventArgs e)
         {
             
-            initCheck();
-            checkRUnit();
-            checkREnabel();
+            InitCheck();
+            CheckRUnit();
+            CheckREnabel();
             
-            textBoxR.Text = calcRes().ToString();
+            textBoxR.Text = CalcRes().ToString();
             
 
         }
 
-        private void checkREnabel()
+        private void CheckREnabel()
         {
             if (textBoxR1.Text == "" || textBoxR1.Text == "0")
-                r1.enable = false;
+                _r1.Enable = false;
             if (textBoxR2.Text == "" || textBoxR2.Text == "0")
-                r2.enable = false;
+                _r2.Enable = false;
             if (textBoxR3.Text == "" || textBoxR3.Text == "0")
-                r3.enable = false;
+                _r3.Enable = false;
             if (textBoxR4.Text == "" || textBoxR4.Text == "0")
-                r4.enable = false;
+                _r4.Enable = false;
             if (textBoxR5.Text == "" || textBoxR5.Text == "0")
-                r5.enable = false;
-            if (r1.enable)
-                r1.value = 1.0 / int.Parse(textBoxR1.Text);
-            if (r2.enable)
-                r2.value = 1.0 / int.Parse(textBoxR2.Text);
-            if (r3.enable)
-                r3.value = 1.0 / int.Parse(textBoxR3.Text);
-            if (r4.enable)
-                r4.value = 1.0 / int.Parse(textBoxR4.Text);
-            if (r5.enable)
-                r5.value = 1.0 / int.Parse(textBoxR5.Text);
+                _r5.Enable = false;
+            if (_r1.Enable)
+                _r1.Value = 1.0 / int.Parse(textBoxR1.Text);
+            if (_r2.Enable)
+                _r2.Value = 1.0 / int.Parse(textBoxR2.Text);
+            if (_r3.Enable)
+                _r3.Value = 1.0 / int.Parse(textBoxR3.Text);
+            if (_r4.Enable)
+                _r4.Value = 1.0 / int.Parse(textBoxR4.Text);
+            if (_r5.Enable)
+                _r5.Value = 1.0 / int.Parse(textBoxR5.Text);
 
         }
 
-        private double calcRes()
+        private double CalcRes()
         {
-            calcUnitedRes(ref r1);
-            calcUnitedRes(ref r2);
-            calcUnitedRes(ref r3);
-            calcUnitedRes(ref r4);
-            calcUnitedRes(ref r5);
+            calcUnitedRes(ref _r1);
+            calcUnitedRes(ref _r2);
+            calcUnitedRes(ref _r3);
+            calcUnitedRes(ref _r4);
+            calcUnitedRes(ref _r5);
             double r;
-            r = 1.0 / (r1.value + r2.value + r3.value + r4.value + r5.value);
+            r = 1.0 / (_r1.Value + _r2.Value + _r3.Value + _r4.Value + _r5.Value);
             return r;
         }
 
-        private void initCheck()
+        private void InitCheck()
         {
-            r1.enable = true;
-            r2.enable = true;
-            r3.enable = true;
-            r4.enable = true;
-            r5.enable = true;
-            r1.value = 0;
-            r2.value = 0;
-            r3.value = 0;
-            r4.value = 0;
-            r5.value = 0;
+            _r1.Enable = true;
+            _r2.Enable = true;
+            _r3.Enable = true;
+            _r4.Enable = true;
+            _r5.Enable = true;
+            _r1.Value = 0;
+            _r2.Value = 0;
+            _r3.Value = 0;
+            _r4.Value = 0;
+            _r5.Value = 0;
         }
 
-        private void  initOmegaOption()
+        private void  InitOmegaOption()
         {
             comboBoxOmegaType1.SelectedIndex = 1;
             comboBoxOmegaType2.SelectedIndex = 1;
@@ -187,36 +187,36 @@ namespace MCUAssistant
             comboBoxOmegaType5.SelectedIndex = 1;
         }
 
-        private void checkRUnit()
+        private void CheckRUnit()
         {
-            r1.unit = comboBoxOmegaType1.SelectedIndex;
-            r2.unit = comboBoxOmegaType2.SelectedIndex;
-            r3.unit = comboBoxOmegaType3.SelectedIndex;
-            r4.unit = comboBoxOmegaType4.SelectedIndex;
-            r5.unit = comboBoxOmegaType5.SelectedIndex;
+            _r1.Unit = comboBoxOmegaType1.SelectedIndex;
+            _r2.Unit = comboBoxOmegaType2.SelectedIndex;
+            _r3.Unit = comboBoxOmegaType3.SelectedIndex;
+            _r4.Unit = comboBoxOmegaType4.SelectedIndex;
+            _r5.Unit = comboBoxOmegaType5.SelectedIndex;
         }
 
         private void calcUnitedRes(ref Res inRes)
         {
-            switch (inRes.unit)
+            switch (inRes.Unit)
             {
                 case 0 :
-                    inRes.value *= 1000;
+                    inRes.Value *= 1000;
                     break;
                 case 1:
                     break;
                 case 2:
-                    inRes.value /= 1000;
+                    inRes.Value /= 1000;
                     break;
                 case 3:
-                    inRes.value /= 1000000;
+                    inRes.Value /= 1000000;
                     break;
                 default:
                     break;
             }
         }
 
-        private void initSerialSetup()
+        private void InitSerialSetup()
         {
             INIFILE.Profile.LoadProfile();//加载所有
 
@@ -333,7 +333,8 @@ namespace MCUAssistant
             }
 
             //串口设置默认选择项
-            cbSerial.SelectedIndex = 0;         //note：获得COM9口，但别忘修改
+            if (cbSerial.Items.Count>0)
+                cbSerial.SelectedIndex = 0;         //note：获得COM9口，但别忘修改
             //cbBaudRate.SelectedIndex = 5;
             // cbDataBits.SelectedIndex = 3;
             // cbStop.SelectedIndex = 0;
@@ -441,9 +442,9 @@ namespace MCUAssistant
                 //处理数字转换
                 string sendBuf = strSend;
                 string sendnoNull = sendBuf.Trim();
-                string sendNOComma = sendnoNull.Replace(',', ' ');    //去掉英文逗号
-                string sendNOComma1 = sendNOComma.Replace('，', ' '); //去掉中文逗号
-                string strSendNoComma2 = sendNOComma1.Replace("0x", "");   //去掉0x
+                string sendNoComma = sendnoNull.Replace(',', ' ');    //去掉英文逗号
+                string sendNoComma1 = sendNoComma.Replace('，', ' '); //去掉中文逗号
+                string strSendNoComma2 = sendNoComma1.Replace("0x", "");   //去掉0x
                 strSendNoComma2.Replace("0X", "");   //去掉0X
                 string[] strArray = strSendNoComma2.Split(' ');
 
